f703213f047a7054243dfb2c9a850b5df43aab1d
kaiqiang2018 <hekq2018@163.com>
2020-08-25 21:18:37 +0800

    dev: 定义分布式锁接口

---
+++ distributed-lock/src/test/java/com/kaiqiang/learn/distributed/lock/LockTest.java
@@ -0,0 +1,26 @@
+package com.kaiqiang.learn.distributed.lock;
+
+import junit.framework.TestCase;
+
+public class LockTest extends TestCase {
+
+    /**
+     * 测试是否对资源上锁
+     */
+    public void testLock() {
+
+
+    }
+    /**
+     * 可重入性测试
+     */
+
+    /**
+     * 性能测试
+     */
+
+    /**
+     *
+     */
+
+}
