bd4748169d91193bcbacc9c23eeb8b4693a5356b
kaiqiang2018 <hekq2018@163.com>
2020-08-30 15:12:41 +0800

    init

---
+++ distributed-lock/src/main/java/com/kaiqiang/learn/distributed/lock/LockApplication.java
@@ -0,0 +1,17 @@
+package com.kaiqiang.learn.distributed.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author kaiqiang
+ * @Date 2020/08/30
+ */
+@SpringBootApplication
+public class LockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockApplication.class, args);
+    }
+
+}
